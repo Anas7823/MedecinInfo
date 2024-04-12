03d921a85de9cfc6b2dae44570d2648c9c967676
Anas7823 <anaselkhiat78@gmail.com>
2024-04-12 12:41:10 +0200

    page un patient

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -6,15 +6,39 @@ import axios from "axios";
 
 
 const Patient = () => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
   let  id  = useParams();
-  const [patient, setPatient] = useState(null);
+  const [patient, setPatient] = useState([]);
+  const [traitement, setTraitement] = useState([]);
+  const [rdvsPatient, setRdvsPatient] = useState([]);
 
   useEffect(() => {
+    console.log("DKLqsdqsjdkqsjdlkjqsldjlkqsjdjqslkdjlqsS");
+    console.log(localStorage.getItem("id_medecin"));
+    console.log("DKLqsdqsjdkqsjdlkjqsldjlkqsjdjqslkdjlqsS");
+
     const fetchPatientDetails = async () => {
+      console.log("ID = ", id.idPatient);
       try {
-        const response = await axios.get(`http://localhost:8000/admin/medecin/${id}/patients`);
+        const response = await axios.post(`http://localhost:8000/medecin/patients/${id.idPatient}`, {'id_medecin': localStorage.getItem("id_medecin")});
         setPatient(response.data);
-        console.log("Patient details", response.data);
+        console.log("Patient details ", response.data);
+
+        const traitement = await axios.post(`http://localhost:8000/patient/${id.idPatient}/traitements`, {'id_medecin': localStorage.getItem("id_medecin")});
+        setTraitement(traitement.data);
+        console.log("Traitement = ", traitement.data);
+
+        const rdvs = await axios.post(`http://localhost:8000/patient/${id.idPatient}/rdvs`, {'id_medecin': localStorage.getItem("id_medecin")});
+        setRdvsPatient(rdvs.data);
+        console.log("RDVs = ", rdvs.data);
+
       } catch (error) {
         console.error("Erreur chargement des détails du patient", error);
       }
@@ -35,32 +59,68 @@ const Patient = () => {
         <Table bordered hover>
           <thead>
             <tr>
-              <th>id</th>
               <th>Nom</th>
               <th>Prenom</th>
               <th>Maladie</th>
               <th>Traitement</th>
               <th>Rdv</th>
               <th>Historique</th>
+              <th>Actions</th>
+
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>{patient.id}</td>
               <td>{patient.nom}</td>
               <td>{patient.prenom}</td>
-              <td>{patient.maladie}</td>
-              <td>{patient.traitement}</td>
-              <td>{patient.rendez_vous}</td>
-              <td>{patient.historique}</td>
+              {traitement && traitement.length > 0 ? (
+                <>
+                  <td>{traitement[0].maladie}</td>
+                  <td>{traitement[0].medicaments}</td>
+                  <td>
+                    {rdvsPatient && rdvsPatient.length > 0 ? (
+                      <>{formatDate(rdvsPatient[0].date)} à {rdvsPatient[0].heure}</>
+                    ) : (
+                      <> </>
+                    )}
+                  </td>
+                  <td>
+                    {traitement.map((traitement, index) => (
+                      <div key={index}>
+                        <p>{traitement.maladie}</p>
+                        <p>{traitement.medicaments}</p>
+                      </div>
+                    ))}
+                  </td>
+                </>
+              ) : (
+                <> </>
+              )}
+              <td style={{display: "flex", flexDirection: "column"}}>
+                <button style={{margin: "5px", backgroundColor: "blue", color: "white", border: 'none', borderRadius: '5px'}}>Modifier</button>
+                <button style={{margin: "5px", backgroundColor: "red", color: "white", border: 'none', borderRadius: '5px'}}>Supprimer</button>
+              </td>
+
             </tr>
           </tbody>
+
         </Table>
       </div>
 
+      <div style={{width: "80vw", margin: "0 auto"}}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h3>Proposer un rendez vous:</h3>
+        <form>
+          <input type="date" name="date" />
+          <input type="time" name="heure" />
+          <button style={{margin: "5px", backgroundColor: "blue", color: "white", border: 'none', borderRadius: '5px'}}>Proposer</button>
+        </form>
+      </div>
+
       <br />
-      <button>Modifier</button>
-      <button>Supprimer</button>
     </div>
   );
 };
